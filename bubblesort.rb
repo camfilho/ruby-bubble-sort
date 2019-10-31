e76bdd32e19582d49d93b5f66a48b11834bfd893
camfilho <cfmedeiros@ufn.edu.br>
2019-10-31 19:54:11 -0300

    improved bubble_sorte

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,9 +1,13 @@
 def bubble_sort(_arr)
     arr = _arr.dup
- (arr.length-1).times do
+    sorted = false
+   until(sorted) 
+     sorted = true
     for i in 0...arr.length-1 do
+      #swaping
         if arr[i] > arr[i+1]
            arr[i], arr[i+1] = arr[i+1], arr[i]
+           sorted = false
           end
       end
     end    
