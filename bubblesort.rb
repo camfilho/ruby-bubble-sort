8509149dab46f5d345717c48adc543f7c4de33c5
camfilho <cfmedeiros@ufn.edu.br>
2019-11-01 16:58:04 -0300

    add bubble_sort_by

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,16 +1,47 @@
 def bubble_sort(_arr)
-    return _arr unless _arr.is_a? Array
-    arr = _arr.dup
-    sorted = false
-   until(sorted) 
-     sorted = true
+  return _arr unless _arr.is_a? Array
+  arr = _arr.dup
+  sorted = false
+  until(sorted) 
+    sorted = true
     for i in 0...arr.length-1 do
       #swaping
-        if arr[i] > arr[i+1]
-           arr[i], arr[i+1] = arr[i+1], arr[i]
-           sorted = false
-          end
+      if arr[i] > arr[i+1]
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        sorted = false
       end
-    end    
-    return arr
+    end
+  end    
+  return arr
 end
+
+
+def bubble_sort_by(_arr)
+  return _arr unless _arr.is_a? Array
+  arr = _arr.dup
+  sorted = false
+  until(sorted) 
+    sorted = true
+    for i in 0...arr.length-1 do
+      result_block = yield(arr[i], arr[i+1])
+      if result_block > 0
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        sorted = false
+      end
+    end
+  end    
+  return arr 
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
