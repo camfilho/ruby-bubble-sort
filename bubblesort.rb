77927797746e5966b17dfcc36f6fcab5ea049150
Luis Puente <puente.luism@gmail.com>
2019-11-01 14:21:20 -0600

    Fix stickler errors

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,47 +1,37 @@
-def bubble_sort(_arr)
-  return _arr unless _arr.is_a? Array
-  arr = _arr.dup
+# frozen_string_literal: true
+
+def bubble_sort(arr2)
+  return arr2 unless arr2.is_a? Array
+
+  arr = arr2.dup
   sorted = false
-  until(sorted) 
+  until (sorted)
     sorted = true
-    for i in 0...arr.length-1 do
-      #swaping
-      if arr[i] > arr[i+1]
-        arr[i], arr[i+1] = arr[i+1], arr[i]
+    (0...arr.length - 1).each do |i|
+      # swaping
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
         sorted = false
       end
     end
-  end    
-  return arr
+  end
+  arr
 end
 
+def bubble_sort_by(arr2)
+  return arr2 unless arr2.is_a? Array
 
-def bubble_sort_by(_arr)
-  return _arr unless _arr.is_a? Array
-  arr = _arr.dup
+  arr = _arr2.dup
   sorted = false
-  until(sorted) 
+  until (sorted)
     sorted = true
-    for i in 0...arr.length-1 do
-      result_block = yield(arr[i], arr[i+1])
-      if result_block > 0
-        arr[i], arr[i+1] = arr[i+1], arr[i]
+    (0...arr.length - 1).each do |i|
+      result_block = yield(arr[i], arr[i + 1])
+      if result_block.positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
         sorted = false
       end
     end
-  end    
-  return arr 
+  end
+  arr
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
